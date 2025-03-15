4d37ba641677589c408c430b3d2b5bbceec94c30
JinGoo <zzgg3040@naver.com>
2025-03-15 15:54:32 +0900

    feat:connect-docker-database

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,4 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+// $ npm install --save @nestjs/typeorm typeorm pg
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('POSTGRES_HOST'),
+        port: configService.get<number>('POSTGRES_PORT'),
+        username: configService.get<string>('POSTGRES_USER'),
+        password: configService.get<string>('POSTGRES_PASSWORD'),
+        database: configService.get<string>('POSTGRES_DB'),
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+  ],
+})
 export class DatabaseModule {}
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 8000);
+  await app.listen(process.env.PORT ?? 8001);
 }
 void bootstrap();
+
+/* 
+os 경로 문제 발생 시
+$ pwd
+$ sudo chown -R 'whoami' ${경로}
+*/
